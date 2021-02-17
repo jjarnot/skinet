4a7c6159fa5738d85e16bf4835fbbf3f9fed2377
Jaroslaw Jarnot <jaroslaw.jarnot@gmail.com>
2021-02-18 00:44:08 +0100

    End of section 6

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -31,6 +31,11 @@ namespace Api
                 x.UseSqlite(_configuration.GetConnectionString("DefaultConnection")));
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,7 +50,7 @@ namespace Api
 
             app.UseRouting();
             app.UseStaticFiles();
-
+            app.UseCors("CorsPolicy");
             app.UseAuthorization();
             app.UseSwaggerDocumention();
 
--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -18,9 +18,36 @@ namespace Core.Specifications
         public Expression<Func<T,bool>> Criteria {get;}
         public List<Expression<Func<T,object>>> Includes {get;} = new List<Expression<Func<T, object>>>();
 
+        public Expression<Func<T, object>> OrderBy {get; private set;}
+
+        public Expression<Func<T, object>> OrderByDescending {get; private set;}
+
+        public int Take {get; private set;}
+
+        public int Skip {get; private set;}
+
+        public bool IsPagindEnabled {get; private set;}
+
         protected void AddInclude(Expression<Func<T, object>> includeEpression)
         {
             Includes.Add(includeEpression);
         }
+
+        protected void AddOrderBy(Expression<Func<T, object>> orderByEpression)
+        {
+            OrderBy = orderByEpression;
+        }      
+
+        protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescEpression)
+        {
+            OrderByDescending = orderByDescEpression;
+        }   
+
+        protected void ApplyPaging(int skip, int take)
+        {
+            Skip = skip;
+            Take = take;
+            IsPagindEnabled = true;
+        }         
     }
 }
